9c2c4572c0f299166d9b93f437e1f7f0cc65b6c6
Nicholas Baker <ndb180002@utdallas.edu>
2022-09-19 08:57:28 -0500

    Node code changes
    - move node code into `run` function so that the main func is brief and only
    parses arguments.
    - remove threads spawn from client connections, and store the sockets within a
    `synchronized` code block.

--- project1/src/Node.java
+++ project1/src/Node.java
@@ -2,6 +2,8 @@ import java.net.Socket;
 import java.net.ServerSocket;
 import java.util.List;
 import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 
 public class Node {
@@ -38,35 +40,43 @@ public class Node {
   }
 
   public static void main(String[] args) throws Exception {
-    /**
-      Pseudocode
-
-      1. read the config file to get the parameters
-      2. spawn the TCP or SCTP server connection handler in the background
-      3. establish connections to other nodes based on configuration file parameters
-      4. handle incoming messages from peers in a state-machine style
-     */
-
     // find out which node this instance is labelled
     final int id = Integer.parseInt(args[1]);
     // read the configuration
-    Config config = Config.fromFile(args[0]);
+    final Config config = Config.fromFile(args[0]);
     // pull the current node's config based on id label
     final Node node = config.nodeConfigs[id];
+    // run the node with knowledge from the configuration file
+    node.run(config);
+  }
+
+  private void run(final Config config) throws Exception {
+
+    int queuedMessages = 0;
+    final Socket[] connections = new Socket[config.nodes];
 
     // spawn a thread to accept connections to this node
     new Thread(() -> {
       try {
         // open a listening socket for the server
-        final ServerSocket welcomeSocket = new ServerSocket(node.getListenPort());
+        final ServerSocket welcomeSocket = new ServerSocket(getListenPort());
         while (true) {
           // accept incoming socket connection requests
           final Socket connectionSocket = welcomeSocket.accept();
           // spawn a new handler for the accepted
-          new Thread(() -> { }).start();
+          new Thread(() -> {
+            try {
+               final BufferedReader reader = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+               // when a message is recieved, buffer it and mark the node as active 
+               
+            } catch (Exception e) {
+              err("failed to spawn handler for connection");
+              e.printStackTrace();
+            }
+          }).start();
         }
       } catch (Exception e) {
-        err(id, "failed to start listening on " + node.getListenPort());
+        err("failed to start listening on " + getListenPort());
         e.printStackTrace();
       }
     }).start();
@@ -75,31 +85,35 @@ public class Node {
     Thread.sleep(3000); // 3 seconds
 
     // create a socket connection to each node in the neighbor set
-    for (int neighborIndex : node.neighbors) {
-      new Thread(() -> {
-        try {
-          final Socket clientSocket = new Socket(
+    for (int neighborIndex : neighbors) {
+      try {
+        synchronized(connections) {
+          connections[neighborIndex] = new Socket(
             config.nodeConfigs[neighborIndex].getHostName(),
             config.nodeConfigs[neighborIndex].getListenPort()
           );
-          log(id, "connected to " + neighborIndex);
-        } catch (Exception e) {
-          err(id, "failed to connect to node " + neighborIndex +
-            " at " + config.nodeConfigs[neighborIndex].getHostName() +
-            " on port " + config.nodeConfigs[neighborIndex].getListenPort());
-          e.printStackTrace();
         }
-      }).start();
+        log("connected to " + neighborIndex);
+      } catch (Exception e) {
+        err("failed to connect to node " + neighborIndex +
+          " at " + config.nodeConfigs[neighborIndex].getHostName() +
+          " on port " + config.nodeConfigs[neighborIndex].getListenPort());
+        e.printStackTrace();
+      }
     }
   }
 
-  private static void err(final int id, final String message) {
+  private void err(final String message) {
     System.err.println("[" + id + "] " + message);
   }
 
-  private static void log(final int id, final String message) {
+  private void log(final String message) {
     System.out.println("[" + id + "] " + message);
   }
 
   private enum State { ACTIVE, PASSIVE }
+
+  private class Message {
+    public Message() { }
+  }
 }

c5fae51623eb0a06166c05b5de8d6abd1ea37e2a
Nicholas Baker <ndb180002@utdallas.edu>
2022-10-27 22:53:06 -0500

    Add metric file logging for responseTime
    metrics oriented logging will be saved into `metrics.out` and then parsed be an
    external program like a python script to create graphs/visualizations of the
    data.
    
    The lines in the metrics log should follow the form:
    ```
    <n>:<d>:<c> <METRIC_NAME> <VALUE>
    ```
    
    the arguments `n`,`d`,`c` follow from the project documentation as:
    1. number of nodes
    2. inter-request delay time
    3. cs-execution time
    
    `METRIC_NAME` should be one of the following:
    1. messageComplexity
    2. reponseTime
    3. systemThroughput
    
    The saved `VALUE` fields can have either the average or a single instance value
    for the execution of a single node, but when the data is compiled in the final
    step, all values with the same (n,d,c,METRIC_NAME) tuple will be averaged into
    one value.

--- project2/src/Node.java
+++ project2/src/Node.java
@@ -1,11 +1,15 @@
+import java.io.FileWriter;
 import java.util.Random;
 
 public class Node {
-  private final MutualExclusionService meService;
 
   private final int id;
   private final Config config;
-  private final Random random;
+  private final MutualExclusionService meService;
+
+  private final Random random = new Random();
+
+  private int responseTimeAccumulator = 0;
 
   public Node(
     final int id,
@@ -15,7 +19,6 @@ public class Node {
     this.id = id;
     this.config = config;
     this.meService = meService;
-    random = new Random();
   }
 
   public static void main(String[] args) throws Exception {
@@ -32,11 +35,27 @@ public class Node {
       log("sleeping until enter request.." + i);
       Thread.sleep(randomExponentialFromMean(config.interRequestDelay));
       log("requesting entrance to cs.." + i);
+      long startTime = System.currentTimeMillis();
       meService.csEnter();
       log("running cs.." + i);
       Thread.sleep(randomExponentialFromMean(config.csExecutionTime));
       log("exiting cs.." + i);
-      meService.csLeave(i + 1 == config.numRequestsToGenerate); // Tell ME if you are finished when leaving CS
+
+      // add to the total response time accumulator
+      responseTimeAccumulator += System.currentTimeMillis() - startTime;
+
+      final boolean finished = i + 1 == config.numRequestsToGenerate;
+      // NOTE: important to save to file before the csLeave, because the csLeave
+      // may call System.exit() before the final node can finish writing its data.
+      if (finished) {
+        // save the average response time to a file
+        final FileWriter outputWriter = new FileWriter(config.project_path + "/metrics.out", true);
+        final String executionParameters = String.join(":", String.valueOf(config.nodes), String.valueOf(config.interRequestDelay), String.valueOf(config.csExecutionTime));
+        outputWriter.write(executionParameters + " responseTime " + (double) responseTimeAccumulator / config.numRequestsToGenerate + "\n");
+        outputWriter.close();
+      }
+
+      meService.csLeave(finished); // Tell ME if you are finished when leaving CS
     }
   }
 
